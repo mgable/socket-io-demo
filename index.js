3912484d393546e692c72e76c89861da6517047d
Mark Gable <mgable43@gmail.com>
2015-11-14 10:05:34 -0800

    working one way chat

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 "use strict";
 var app = require('express')(),
-	http = require('http').Server(app);
+	http = require('http').Server(app),
+	io = require('socket.io')(http);
 
 app.get("/", function(req,res){
-	res.send("<h1>hello world</h1>");
+	res.sendFile(__dirname + "/index.html");
 });
+
+app.get("/*", function(req,res){
+	console.info("XXXXXX");
+	// console.info(req);
+	res.sendFile(__dirname + req.url)
+});
+
 
 http.listen(3001, function(){
 	console.info('lisitining on port 3001');
 });
+
+io.on('connection', function(socket){
+	console.info('a user connected');
+	socket.on('disconnect', function(){
+		console.info("user disconnected");
+	});
+
+	socket.on('chat message', function(msg){
+		console.info("message: " + msg);
+	});
+});
+
